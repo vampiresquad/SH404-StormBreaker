b5a2255ffdd8cf510b58c61470cdef5ff06e8d2d
Vampire Squad <vampiresquad.org@gmail.com>
2025-05-21 14:14:32 +0600

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -2,6 +2,41 @@ import os
 import time
 from termcolor import cprint
 from pyfiglet import figlet_format
+import shutil
+
+def print_banner():
+    os.system("clear")
+    banner = r'''
+\033[1;91m
+██╗   ██╗ █████╗ ███╗   ███╗██████╗ ██╗██████╗ ███████╗
+██║   ██║██╔══██╗████╗ ████║██╔══██╗██║██╔══██╗██╔════╝
+██║   ██║███████║██╔████╔██║██████╔╝██║██║  ██║█████╗  
+╚██╗ ██╔╝██╔══██║██║╚██╔╝██║██╔═══╝ ██║██║  ██║██╔══╝  
+ ╚████╔╝ ██║  ██║██║ ╚═╝ ██║██║     ██║██████╔╝███████╗
+  ╚═══╝  ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝     ╚═╝╚═════╝ ╚══════╝
+       \033[1;97m=> Coded By: Muhammad Shourov (VAMPIRE)
+'''
+    print(banner)
+
+def notify(msg, color="green"):
+    colors = {"red": "91", "green": "92", "blue": "94", "yellow": "93"}
+    print(f"\n\033[1;{colors.get(color,'92')}m[+]\033[0m \033[1m{msg}\033[0m")
+
+def error(msg):
+    print(f"\033[1;91m[-] Error: {msg}\033[0m")
+
+def warn(msg):
+    print(f"\033[1;93m[!] {msg}\033[0m")
+
+def success(msg):
+    print(f"\033[1;92m[✓] {msg}\033[0m")
+
+def check_tool(name):
+    return shutil.which(name)
+
+def install_tool(name):
+    print(f"\033[1;94m[+] Installing {name}...\033[0m")
+    os.system(f"pkg install {name} -y || apt install {name} -y || apt-get install {name} -y")
 
 def print_banner():
     os.system("clear")
